dc1c29e791105ffab237bde78904c5837136e3c4
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2023-09-17 15:54:35 +0200

    renamed test to playground as I just use the file for testing.

---
+++ playground.py
@@ -0,0 +1,11 @@
+
+myoperator:OP = td.op("foobar")
+myoperator.cookedThisFrame
+
+myoperator.cook
+
+myVector:Vector = tdu.Vector(1,2,3)
+myVector.scale(1,2)
+
+randomOperator:scriptSOP = root.op("Foobar")
+randomOperator.
